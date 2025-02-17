59ac84826bab452def24dd5f263fdb4bd6eeb596
Leonardo <leonardo2.sanches@usp.br>
2025-02-17 16:44:49 -0300

    removed init warning. Subset dataset

--- R/downloadGTF.R
+++ R/downloadGTF.R
@@ -6,23 +6,23 @@
 #'
 #' @return A dataframe with "gene_id" and "gene_name" columns.
 #' @export
-#' @import rtracklayer
+#' @importFrom rtracklayer import
 downloadGTF <- function(link) {
   # Set a longer timeout for large downloads
   options(timeout = 600)
-  
+
   # Import GTF file
   gtf_gr <- rtracklayer::import(link, format = "gtf")
   gtf_df <- as.data.frame(gtf_gr)
-  
+
   # Extract relevant columns
   annotation_df <- gtf_df[, c("gene_id", "gene_name")]
   # Remove version numbers from gene_id and gene_name if present
   annotation_df$gene_id <- sub("\\..*$", "", annotation_df$gene_id)
   annotation_df$gene_name <- sub("\\..*$", "", annotation_df$gene_name)
-  
+
   # Remove duplicate gene_ids
   annotation_df <- annotation_df[!duplicated(annotation_df$gene_id), ]
-  
+
   return(annotation_df)
 }
--- R/loadGTF.R
+++ R/loadGTF.R
@@ -6,20 +6,20 @@
 #'
 #' @return A dataframe with "gene_id" and "gene_name" columns.
 #' @export
-#' @import rtracklayer
+#' @importFrom rtracklayer import
 loadGTF <- function(file) {
   # Import GTF file
   gtf_gr <- rtracklayer::import(file, format = "gtf")
   gtf_df <- as.data.frame(gtf_gr)
-  
+
   # Extract relevant columns
   annotation_df <- gtf_df[, c("gene_id", "gene_name")]
   # Remove version numbers from gene_id and gene_name if present
   annotation_df$gene_id <- sub("\\..*$", "", annotation_df$gene_id)
   annotation_df$gene_name <- sub("\\..*$", "", annotation_df$gene_name)
-  
+
   # Remove duplicate gene_ids
   annotation_df <- annotation_df[!duplicated(annotation_df$gene_id), ]
-  
+
   return(annotation_df)
 }
